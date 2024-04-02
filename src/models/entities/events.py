0740ddcc50834e79ba79aebc54ed99644fc89608
Lugan Costa <luganthierry@hotmail.com>
2024-04-01 22:50:12 -0300

    feat: implementing repository and making tests

--- src/models/entities/events.py
+++ src/models/entities/events.py
@@ -7,4 +7,8 @@ class Events(Base):
     id = Column(String, primary_key=True)
     title = Column(String, nullable=False)
     details = Column(String)
+    slug = Column(String, nullable=False)
     maximum_attendees = Column(Integer)
+
+    def __repr__(self):
+        return f"Events [title={self.title}, maximum_attendees={self.maximum_attendees}]"
---
+++ src/models/repository/events_repository_test.py
@@ -0,0 +1,26 @@
+import pytest
+from src.models.settings.connection import db_connection_handler
+from .events_repository import EventsRepository
+
+db_connection_handler.connect_to_db()
+
+@pytest.mark.skip(reason="Novo registro em banco")
+def test_inser_event():
+    event = {
+        "uuid": "teste123",
+        "title": "o título do meu evento de teste00",
+        "slug": "o slogan do meu evento de teste00",
+        "maximum_attendees": 22
+    }
+
+    events_repository = EventsRepository()
+    response = events_repository.insert_event(event)
+
+    print(response)
+
+def test_get_event_by_id():
+    event_id = "teste123"
+    events_repository = EventsRepository()
+    response = events_repository.get_event_by_id(event_id)
+
+    print(response)
