537970feba1aa5569c913ae74caeef2b35ca7721
Lugan Costa <luganthierry@hotmail.com>
2024-04-04 01:59:07 -0300

    feat: implementing check in routes

--- src/models/repository/check_ins_repository.py
+++ src/models/repository/check_ins_repository.py
@@ -2,12 +2,12 @@ from src.models.settings.connection import db_connection_handler
 from src.models.entities.check_ins import CheckIns
 from sqlalchemy.exc import IntegrityError
 
-class ClackInRepository:
+class CheckInRepository:
     def insert_check_in(self, attendee_id: str) -> str:
         with db_connection_handler as database:
             try:
                 check_in = (   
-                    CheckIns(attendee_id=attendee_id)
+                    CheckIns(attendeeId=attendee_id)
                 )
                 database.session.add(check_in)
                 database.session.commit()
